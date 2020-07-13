e239d35e6e064becd2e753ee1a5b4e77caa17602
Ivana Filipovska <ivanafilipovska996@gmail.com>
2020-07-13 21:28:53 +0200

    Updates

---
+++ precache-manifest.67b22a5bce8face782186ecb2f851f62.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3b183466281c8dbc5fa1cea073ddba",
+    "url": "/React-Carousel/index.html"
+  },
+  {
+    "revision": "e5096e4a8a5d54debd8b",
+    "url": "/React-Carousel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "b8e0c5999df025673e7f",
+    "url": "/React-Carousel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e5096e4a8a5d54debd8b",
+    "url": "/React-Carousel/static/js/2.c108a8ac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Carousel/static/js/2.c108a8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e0c5999df025673e7f",
+    "url": "/React-Carousel/static/js/main.9840bbc0.chunk.js"
+  },
+  {
+    "revision": "19075323b92d401e6ee1",
+    "url": "/React-Carousel/static/js/runtime-main.6c5f7d68.js"
+  },
+  {
+    "revision": "8b8ef8c83390c3b2738805b627b3f708",
+    "url": "/React-Carousel/static/media/bitola.8b8ef8c8.jpg"
+  },
+  {
+    "revision": "ffbe250d356416ef4b59063f2202a19e",
+    "url": "/React-Carousel/static/media/krusevo.ffbe250d.jpg"
+  },
+  {
+    "revision": "7993c0de58793f5c91628c542fc5023a",
+    "url": "/React-Carousel/static/media/ohrid.7993c0de.jpg"
+  },
+  {
+    "revision": "d60f9ff8cfc26309fd59d8e77a0452e8",
+    "url": "/React-Carousel/static/media/skopje.d60f9ff8.jpg"
+  },
+  {
+    "revision": "2d24a06937ea9bf5958515d09b1ed341",
+    "url": "/React-Carousel/static/media/strumica.2d24a069.jpg"
+  },
+  {
+    "revision": "525d3e1f6bb86dce23159a86b61e8cdd",
+    "url": "/React-Carousel/static/media/veles.525d3e1f.jpg"
+  }
+]);
